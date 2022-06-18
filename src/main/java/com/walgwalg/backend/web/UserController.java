8a7475f279e9a30402dfb372be3c718ff197249a
Koonayoung518 <koony000415@gmail.com>
2022-06-18 15:32:50 +0900

    feat : dockerfile 생성

--- src/main/java/com/walgwalg/backend/web/UserController.java
+++ src/main/java/com/walgwalg/backend/web/UserController.java
@@ -78,4 +78,9 @@ public class UserController {
                 .build();
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
+    @GetMapping("/dev")
+    public String dev(){
+        System.out.println("dev 실행");
+        return "Hello world!";
+    }
 }
