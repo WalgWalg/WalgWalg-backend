dcf3bb0e38a66eb188495112a9ead1802e01f199
Koonayoung518 <koony000415@gmail.com>
2022-07-14 18:28:45 +0900

    feat : RDS 연동

--- src/main/resources/db/migration/V202207141__init_table.sql
+++ src/main/resources/db/migration/V202207141__init_table.sql
@@ -1,7 +1,16 @@
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS walk;
+DROP TABLE IF EXISTS gps;
+DROP TABLE IF EXISTS board;
+DROP TABLE IF EXISTS hashtag;
+DROP TABLE IF EXISTS likes;
+DROP TABLE IF EXISTS scrap;
+DROP TABLE IF EXISTS park;
+
 CREATE TABLE users (
     id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     userid VARCHAR(63),
-    password VARCHAR(63),
+    password VARCHAR(255),
     nickname VARCHAR(63),
     address VARCHAR(63),
     salt VARCHAR(63),
@@ -10,8 +19,8 @@ CREATE TABLE users (
 );
 
 CREATE TABLE walk (
-    walk_id VARCHAR(63) NOT NULL,
-    walk_date DATE,
+    walk_id VARCHAR(63) NOT NULL PRIMARY KEY,
+    walk_date TIMESTAMP,
     step_count BIGINT,
     distance BIGINT,
     calorie BIGINT,
@@ -20,7 +29,7 @@ CREATE TABLE walk (
     location VARCHAR(63),
     address VARCHAR(63),
     user_id BIGINT,
-    FOREIGN KEY(user_id) REFERENCES users(id);
+    FOREIGN KEY(user_id) REFERENCES users(id)
 );
 
 CREATE TABLE gps (
@@ -28,41 +37,41 @@ CREATE TABLE gps (
     latitude VARCHAR(63),
     longitude VARCHAR(63),
     walk_id VARCHAR(63),
-    FOREIGN KEY(walk_id) REFERENCES walk(walk_id);
+    FOREIGN KEY(walk_id) REFERENCES walk(walk_id)
 );
 
 CREATE TABLE board (
-    board_id VARCHAR(63) NOT NULL,
+    board_id VARCHAR(63) NOT NULL PRIMARY KEY,
     title VARCHAR(63),
     board_date TIMESTAMP,
     contents VARCHAR(255),
     walk_id VARCHAR(63),
     FOREIGN KEY(walk_id) REFERENCES walk(walk_id),
-    user_id VARCHAR(63),
-    FOREIGN KEY(user_id) REFERENCES users(user_id);
+    user_id BIGINT,
+    FOREIGN KEY(user_id) REFERENCES users(id)
 );
 
 CREATE TABLE hashtag (
     id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     tag VARCHAR(63),
     board_id VARCHAR(63),
-    FOREIGN KEY(board_id) REFERENCES board(board_id);
+    FOREIGN KEY(board_id) REFERENCES board(board_id)
 );
 
 CREATE TABLE likes (
     id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     board_id VARCHAR(63),
     FOREIGN KEY(board_id) REFERENCES board(board_id),
-    user_id VARCHAR(63),
-    FOREIGN KEY(user_id) REFERENCES users(user_id);
+    user_id BIGINT,
+    FOREIGN KEY(user_id) REFERENCES users(id)
 );
 
 CREATE TABLE scrap (
     id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     board_id VARCHAR(63),
     FOREIGN KEY(board_id) REFERENCES board(board_id),
-    user_id VARCHAR(63),
-    FOREIGN KEY(user_id) REFERENCES users(user_id);
+    user_id BIGINT,
+    FOREIGN KEY(user_id) REFERENCES users(id)
 );
 
 CREATE TABLE park (
